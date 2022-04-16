b5702e2867bd7fd2403180b7c4122e9838f3f92d
Roberto Sousa <robertosousa186@gmail.com>
2022-04-16 01:48:13 -0300

    Updating server

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const app = setupApp();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`App running at http:localhost:${PORT}`);
