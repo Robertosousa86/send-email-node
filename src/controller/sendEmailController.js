af2216de7843a6bd874a182b011f5910aa3ce02e
Roberto Sousa <robertosousa186@gmail.com>
2022-04-20 03:12:24 -0300

    Updating sendEmailController

--- src/controller/sendEmailController.js
+++ src/controller/sendEmailController.js
@@ -3,13 +3,13 @@ const sendMessage = require('../provider/mailTrap');
 class sendEmail {
   async postEmail(req, res) {
     try {
-      const { clientEmail, subject, text } = req.body;
+      const { email, subject, text } = req.body;
 
-      await sendMessage(clientEmail, subject, text);
+      await sendMessage(email, subject, text);
 
       return res.status(200).send({ message: 'Email enviado com sucesso!' });
     } catch (err) {
-      return res.status(400).send({ message: 'Erro ao enviar email' });
+      return res.status(400).send({ message: 'Erro ao enviar Email.' });
     }
   }
 }
