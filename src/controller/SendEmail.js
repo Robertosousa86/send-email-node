6ff92a24dc05813698b9683f909f82b46f4aa10e
Roberto Sousa <robertosousa186@gmail.com>
2022-04-28 23:26:55 -0300

    Renaming file

---
+++ src/controller/SendEmail.js
@@ -0,0 +1,18 @@
+const sendMessage = require('../provider/mailTrap');
+const emptyEmailField = require('../error/emptyEmailField');
+
+class SendEmail {
+  async postEmail(req, res) {
+    try {
+      const { email, subject, text } = req.body;
+
+      await sendMessage(email, subject, text);
+
+      return res.status(200).send({ message: 'Email enviado com sucesso!' });
+    } catch (error) {
+      return emptyEmailField(res, error);
+    }
+  }
+}
+
+module.exports = SendEmail;
