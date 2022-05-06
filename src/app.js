3e4f36feb596e394aeac517c7293d142866df89d
Roberto Sousa <robertosousa186@gmail.com>
2022-05-06 00:16:34 -0300

    Updating app

--- src/app.js
+++ src/app.js
@@ -5,6 +5,11 @@ const routes = require('./routes');
 
 const app = express();
 
+app.set('views', './src/views/pages');
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/views'));
+
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
